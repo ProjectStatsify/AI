a8f2982d9d62bed7e363afc7ff8ed41d5a75dc30
JORDAAR <69628820+Jordaar@users.noreply.github.com>
2023-02-15 23:30:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -25,10 +25,11 @@ if (process.env.PUPPETEER_USERDATADIR)
     options.userDataDir = process.env.PUPPETEER_USERDATADIR;
 if (process.env.PUPPETEER_PROXY)
     options.args.push(`--proxy-server=${process.env.PUPPETEER_PROXY}`);
-const browser = await puppeteer.launch(options);
+
+let browser = await puppeteer.launch(options);
+console.log("BROWSER: Process launched");
 
 const BASE_URL = "https://you.com/api/streamingSearch";
-console.log("BROWSER: Process launched");
 
 app.all("/chat", async (req, res) => {
     const query = req.body?.q ?? req.query.q;
@@ -46,43 +47,50 @@ app.all("/chat", async (req, res) => {
 
     const reqURL = `${BASE_URL}?${qs.stringify(vars)}`;
 
-    const page = await browser.newPage();
-    await page.goto(reqURL);
+    try {
+        const page = await browser.newPage();
+        await page.goto(reqURL);
 
-    const pageContent = await page.evaluate(() => document.body.textContent);
+        const pageContent = await page.evaluate(() => document.body.textContent);
 
-    let data = {
-        error: false,
-        content: "",
-        intents: null,
-        requestOptions: vars
+        let data = {
+            error: false,
+            content: "",
+            intents: null,
+            requestOptions: vars
 
-    };
-    const parser = createParser((stream) => {
-        if (stream.type === 'event') {
-            if (stream.event === "youChatToken") {
-                const streamData = JSON.parse(stream.data);
-                data.content += streamData["youChatToken"];
+        };
+        const parser = createParser((stream) => {
+            if (stream.type === 'event') {
+                if (stream.event === "youChatToken") {
+                    const streamData = JSON.parse(stream.data);
+                    data.content += streamData["youChatToken"];
+                }
+                if (stream.event === "intents") {
+                    const streamData = JSON.parse(stream.data);
+                    data.intents = streamData?.intents ?? [];
+                }
+                if (stream.event === "error") {
+                    data.error = true;
+                }
             }
-            if (stream.event === "intents") {
-                const streamData = JSON.parse(stream.data);
-                data.intents = streamData?.intents ?? [];
-            }
-            if (stream.event === "error") {
-                data.error = true;
-            }
-        }
-    });
-    parser.feed(pageContent);
-    parser.reset();
+        });
+        parser.feed(pageContent);
+        parser.reset();
 
-    await page.close();
-    res.send({
-        status: !data.error, data: {
-            ...data,
-            content: data.content.trimStart()
-        }
-    });
+        await page.close();
+        res.send({
+            status: !data.error, data: {
+                ...data,
+                content: data.content.trimStart()
+            }
+        });
+    } catch (error) {
+        res.status(404).send({ status: false, message: "An unexpected error ocurred" });
+        console.log(error);
+        await browser.close().catch((e) => console.log(e));
+        browser = await puppeteer.launch(options);
+    }
 
 });
 
